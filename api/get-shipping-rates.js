538f1bdbf5f48ad5d10575bc042fe285d48b34e6
PERDIGNUS <perdignusllc@gmail.com>
2025-12-03 22:26:06 -0500

    Update get-shipping-rates.js

--- api/get-shipping-rates.js
+++ api/get-shipping-rates.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     try {
         const { customerAddress, product } = req.body;
         const apiKey = process.env.EASYPOST_API_KEY;
@@ -27,12 +27,10 @@ module.exports = async (req, res) => {
             phone: '6167196346'
         };
 
-        // We now build the API request manually and use fetch
         const response = await fetch('https://api.easypost.com/v1/shipments', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                // EasyPost uses Basic Auth with the API key as the username
                 'Authorization': `Basic ${Buffer.from(apiKey + ':').toString('base64')}`
             },
             body: JSON.stringify({
@@ -47,8 +45,7 @@ module.exports = async (req, res) => {
         const data = await response.json();
 
         if (!response.ok) {
-            // If EasyPost returns an error, forward it
-            throw new Error(data.error.message || 'EasyPost API error');
+            throw new Error(data.error?.message || 'EasyPost API error');
         }
 
         return res.status(200).json(data.rates);
@@ -57,4 +54,4 @@ module.exports = async (req, res) => {
         console.error("Function Error:", error);
         return res.status(500).json({ error: error.message });
     }
-};
+}
