aa09711ca38707f999e39637255b83349dbdfffd
PERDIGNUS <perdignusllc@gmail.com>
2025-12-21 11:59:29 -0500

    Update product.js

--- product.js
+++ product.js
@@ -4,7 +4,7 @@ const PRODUCTS = [
         priceId: "", // Leave blank for now
         name: "Summer Sun Stool", 
         price: "Coming Soon", 
-        image: "images/product-summer sun.jpg",
+        image: "images/product-summer-sun.jpg",
         isNewMember: true,
         isAvailable: false // Set to true only when Stripe is ready
     }
